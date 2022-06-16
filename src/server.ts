1864424b84bb5b329bb554c52753a024f1a6bbd3
Ana Luisa <ana.luisa.isa@hotmail.com>
2022-06-15 23:39:32 -0300

    Criação: Rota para motorcycle

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,20 @@ import CarController from './controllers/Car';
 import { Car } from './interfaces/CarInterface';
 import CustomRouter from './routes/Router';
 import App from './app';
+import MotorcycleController from './controllers/Motorcycle';
+import { Motorcycle } from './interfaces/MotorcycleInterface';
 
 const server = new App();
 
 const carController = new CarController();
-
 const carRouter = new CustomRouter<Car>();
 carRouter.addRoute(carController);
 
+const motorcycleController = new MotorcycleController();
+const motorcycleRouter = new CustomRouter<Motorcycle>();
+motorcycleRouter.addRoute(motorcycleController);
+
 server.addRouter(carRouter.router);
+server.addRouter(motorcycleRouter.router);
 
 export default server;
