36e5f13741c24794f2621b996d81086ca9b24a24
Ana Luisa <ana.luisa.isa@hotmail.com>
2022-06-15 23:38:00 -0300

    Criação: Cria Model Motorcycle no mongoDB com tipos corretos

---
+++ src/models/Motorcycle.ts
@@ -0,0 +1,21 @@
+import { Document, Schema, model as createModel } from 'mongoose';
+import { Motorcycle } from '../interfaces/MotorcycleInterface';
+import MongoModel from './MongoModel';
+
+interface MotorcycleDocument extends Motorcycle, Document { }
+
+const motorcycleSchema = new Schema<MotorcycleDocument>({
+  model: String,
+  year: Number,
+  color: String,
+  status: Boolean,
+  buyValue: Number,
+  category: String,
+  engineCapacity: Number,
+}, { versionKey: false });
+
+export default class MotorcyleModel extends MongoModel<Motorcycle> {
+  constructor(model = createModel('Motorcycles', motorcycleSchema)) {
+    super(model);
+  }
+}
